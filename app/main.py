1fc4ca1ecb112bc9584884e6358d751eac3a7dac
bipsec <bip.sec22@gmail.com>
2023-08-02 19:19:03 +0600

    updated file structure

--- app/main.py
+++ app/main.py
@@ -1,67 +1,18 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
+from app.api import dictionary
 
 app = FastAPI()
 
-# Load the CSV data into a DataFrame
-data = pd.read_csv("app/data/demo_data.csv")
-data = data.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+
+# Demo api testing
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Bangla Dictionary API. It provides data for Bangla Dictionary Web Application."}
 
 
-@app.get("/dictionary/words/")
-async def get_similar_spellings(word: str):
-    try:
-        # Filter the rows where the word matches the input word
-        filtered_data = data[data["word"] == word]
-
-        # Group the data by 'number' and 'meaning' columns
-        grouped_data = filtered_data.groupby(["number", "meaning"])["word"].count().reset_index()
-
-        # Convert the grouped data to the desired JSON format
-        similar_spellings = []
-        for idx, row in grouped_data.iterrows():
-            similar_spellings.append({
-                "id": idx + 1,
-                "meaning_no": row["number"],
-                "meanings": [row["meaning"]]
-            })
-
-        response = {
-            "similar_spellings": similar_spellings,
-            "word": word
-        }
-        return response
-    except Exception as e:
-        # Log the error for debugging purposes
-        print("Error:", e)
-        # Raise an HTTPException with a 500 status code to indicate a server error
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
-
-@app.get("/dictionary/all_words/")
-async def get_all_words():
-    all_words = data["word"].unique().tolist()
-
-    all_responses = []
-    for word in all_words:
-        similar_spellings = []
-        filtered_data = data[data["word"] == word]
-        grouped_data = filtered_data.groupby(["number", "meaning"])["word"].count().reset_index()
-        for idx, row in grouped_data.iterrows():
-            similar_spellings.append({
-                "id": idx + 1,
-                "meaning_no": row["number"],
-                "meanings": [row["meaning"]]
-            })
-
-        response = {
-            "similar_spellings": similar_spellings,
-            "word": word
-        }
-        all_responses.append(response)
-
-    return all_responses
+# Including API Router
+app.include_router(dictionary.router)
 
 
 # Configure CORS middleware to allow requests from all origins
