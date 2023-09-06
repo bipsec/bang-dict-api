52714bde4b3ae7acc92c52ef20e31fee19ae2ccc
bipsec <bip.sec22@gmail.com>
2023-09-07 01:39:38 +0600

    feat: CORS issue fixed

--- app/main.py
+++ app/main.py
@@ -1,20 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from pathlib import Path
+import csv
 from app.api import dictionary
+from app.db.database import SessionLocal, Base
+from app.db.models import WordMeaning, Base
+from app.db.database import get_db
 
 app = FastAPI()
 
+# Including API Router
+app.include_router(dictionary.router)
 
-# Demo api testing
+# CORS settings
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to Bangla Dictionary API. It provides data for Bangla Dictionary Web Application."}
 
 
-# Including API Router
-app.include_router(dictionary.router)
 
-
-# Configure CORS middleware to allow requests from all origins
-origins = ["*"]
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"])
