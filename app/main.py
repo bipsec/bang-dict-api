512ecd29839b5f653ea6f0cf4267c681621f22c1
bipsec <bip.sec22@gmail.com>
2023-10-09 16:10:31 +0600

    fix: main function updated

--- app/main.py
+++ app/main.py
@@ -3,17 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from pathlib import Path
 import csv
-from app.api import dictionary, ipa
+from app.api import dictionary, ipa, load_data, load_audio
 
-from app.db.database import SessionLocal, Base
-# from app.db.models import WordMeaning, Base
-from app.db.database import get_db, WordMeaning
+# from app.db.database import SessionLocal, Base, get_db, WordMeaning
 
 app = FastAPI()
 
 # Including API Router
 app.include_router(dictionary.router)
 app.include_router(ipa.router)
+app.include_router(load_data.router)
+app.include_router(load_audio.router)
 
 # CORS settings
 origins = ["*"]
@@ -32,40 +32,4 @@ async def root():
     return {"message": "Welcome to Bangla Dictionary API. It provides data for Bangla Dictionary Web Application."}
 
 
-@app.post("/load-csv/")
-async def load_csv_to_db(db: Session = Depends(get_db)):
-    csv_path = Path("app/data/bangla_dictionary.csv")
-    if not csv_path.exists():
-        raise HTTPException(status_code=404, detail="CSV file not found")
 
-    with open(csv_path, newline="", encoding="utf-8") as csvfile:
-        csvreader = csv.DictReader(csvfile)
-        for row in csvreader:
-            # if any(value is None or value.strip() == '' for value in row.values()):
-            #     continue
-
-
-            try:
-                # Ensure that the keys in 'row' match the model column names
-                word_meaning = WordMeaning(
-                    pageNo=row["pageNo"],
-                    words=row['word'],
-                    number=row["number"],
-                    spelling=row['pronunciation'],
-                    meaning=row['meaning'],
-                    pos=row["pos"],
-                    ipa=row['IPA'],
-                    root_lang=row['language'],
-                    type=row['class'],
-                    sentence=row['sentence'],
-                    source=row['source'],
-                    audio=row["Audio"]
-                )
-                db.add(word_meaning)
-            except Exception as e:
-                # Handle any exceptions here (e.g., data type mismatch)
-                db.rollback()
-                print(f"Error: {str(e)}")
-        db.commit()
-
-    return {"message": "CSV data loaded successfully"}
