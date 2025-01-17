ae56952088fa84f1f3482d4ba65213ef0375c626
bipsec <bip.sec22@gmail.com>
2025-01-17 17:21:25 +0900

    db connection configured

--- app/db/database.py
+++ app/db/database.py
@@ -3,81 +3,20 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
+from app.core.config import get_settings
+from app.db.models import Base
 
+settings = get_settings()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db:5432/postgres"
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=0
+)
 
-Base = declarative_base()
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-
-class WordMeaning(Base):
-    __tablename__ = "word_meaning"
-
-    id = Column(Integer, primary_key=True, index=True)
-    pageNo = Column(String, unique=False, index=True)
-    words = Column(String, unique=False, index=True)
-    number = Column(String, unique=False, nullable=True, index=True)
-    spelling = Column(String, nullable=True, index=True)
-    meaning = Column(String, nullable=True, index=True)
-    pos = Column(String, nullable=True, unique=False, index=True)
-    ipa = Column(String, unique=False, nullable=True, index=True)
-    root_lang = Column(String, nullable=True, index=True)
-    type = Column(String, unique=False, nullable=True, index=True)
-    sentence = Column(String, unique=False, nullable=True, index=True)
-    source = Column(String, index=True)
-
-    sentences = relationship("Sentence", back_populates="word_meaning")
-    pos_taggers = relationship("PosTagger", back_populates="word_meaning")
-    ipas = relationship("IPA", back_populates="word_meaning")
-    audio_data = relationship("AudioData", back_populates="word_meaning")
-
-
-class Sentence(Base):
-    __tablename__ = "word_sentence"
-
-    id = Column(Integer, primary_key=True, index=True)
-    words = Column(String, unique=False, index=True)
-    sentence = Column(String, unique=False, nullable=True, index=True)
-    word_meaning_id = Column(Integer, ForeignKey("word_meaning.id"))
-
-    word_meaning = relationship("WordMeaning", back_populates="sentences")
-
-
-class PosTagger(Base):
-    __tablename__ = "word_pos"
-
-    id = Column(Integer, primary_key=True, index=True)
-    words = Column(String, unique=False, index=True)
-    pos = Column(String, unique=False, nullable=True, index=True)
-    word_meaning_id = Column(Integer, ForeignKey("word_meaning.id"))
-
-    word_meaning = relationship("WordMeaning", back_populates="pos_taggers")
-
-
-class IPA(Base):
-    __tablename__ = "ipa"
-
-    id = Column(Integer, primary_key=True, index=True)
-    words = Column(String, unique=False, index=True)
-    ipa = Column(String, unique=False, nullable=True, index=True)
-    word_meaning_id = Column(Integer, ForeignKey("word_meaning.id"))
-
-    word_meaning = relationship("WordMeaning", back_populates="ipas")
-
-
-class AudioData(Base):
-    __tablename__ = "word_audio"
-
-    id = Column(Integer, primary_key=True, index=True)
-    filename = Column(String, unique=False, nullable=True)
-    duration = Column(Float, unique=False, nullable=True)
-    audio_file = Column(LargeBinary, unique=False, nullable=True)
-    audio_type = Column(String, unique=False, nullable=True)
-    word_meaning_id = Column(Integer, ForeignKey("word_meaning.id"))
-
-    word_meaning = relationship("WordMeaning", back_populates="audio_data")
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:ariake@db:5432/postgres"
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/db/models.py
+++ app/db/models.py
@@ -1,57 +1,74 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, LargeBinary, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-# Base = declarative_base()
+Base = declarative_base()
 
 
-# class WordMeaning(Base):
-#     __tablename__ = "word_meaning"
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     pageNo = Column(String, unique=True, index=True)
-#     words = Column(String, unique=False, index=True)
-#     number = Column(String, nullable=True, index=True)
-#     spelling = Column(String, nullable=True, index=True)
-#     meaning = Column(String, nullable=True, index=True)
-#     pos = Column(String, nullable=True, unique=False, index=True)
-#     ipa = Column(String, unique=False, nullable=True, index=True)
-#     root_lang = Column(String, nullable=True, index=True)
-#     type = Column(String, unique=False, nullable=True, index=True)
-#     sentence = Column(String, unique=False, nullable=True,index=True)
-#     source = Column(String, index=True)
-#     audio = Column(String, nullable=True, index=True)
+class WordMeaning(Base):
+    __tablename__ = "word_meaning"
 
-#     ipa = relationship("IPA", back_populates="word_meaning")
-#     sentences = relationship("Sentence", back_populates="word_meaning")
-#     pos_taggers = relationship("PosTagger", back_populates="word_meaning")
-#
-#
-# class IPA(Base):
-#     __tablename__ = "ipa"
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     ipa = Column(String, index=True)
-#     words_id = Column(String, ForeignKey("word_meaning.words"))
-#
-#     word_meaning = relationship("WordMeaning", back_populates="ipa")
-#
-#
-# class PosTagger(Base):
-#     __tablename__ = "parts_of_speech"
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     pos = Column(String, index=True)
-#     words_id = Column(String, ForeignKey('word_meaning.words'))
-#
-#     word_meaning = relationship("WordMeaning", back_populates="pos_taggers")
-#
-#
-# class Sentence(Base):
-#     __tablename__ = "sentence"
-#
-#     id = Column(Integer, primary_key=True, index=True)
-#     sentence = Column(String, index=True)
-#     words_id = Column(String, ForeignKey("word_meaning.words"))
-#
-#     word_meaning = relationship("WordMeaning", back_populates="sentences")
+    id = Column(Integer, primary_key=True, index=True)
+    pageNo = Column(String, unique=False, index=True)
+    words = Column(String, unique=False, index=True)
+    number = Column(String, unique=False, nullable=True, index=True)
+    spelling = Column(String, nullable=True, index=True)
+    meaning = Column(String, nullable=True, index=True)
+    pos = Column(String, nullable=True, unique=False, index=True)
+    ipa = Column(String, unique=False, nullable=True, index=True)
+    root_lang = Column(String, nullable=True, index=True)
+    type = Column(String, unique=False, nullable=True, index=True)
+    sentence = Column(String, unique=False, nullable=True, index=True)
+    source = Column(String, index=True)
+
+    sentences = relationship("Sentence", back_populates="word_meaning")
+    pos_taggers = relationship("PosTagger", back_populates="word_meaning")
+    ipas = relationship("IPA", back_populates="word_meaning")
+    audio_data = relationship("AudioData", back_populates="word_meaning")
+
+
+class Sentence(Base):
+    __tablename__ = "word_sentence"
+
+    id = Column(Integer, primary_key=True, index=True)
+    words = Column(String, unique=False, index=True)
+    sentence = Column(String, unique=False, nullable=True, index=True)
+    word_meaning_id = Column(Integer, ForeignKey("word_meaning.id"))
+
+    word_meaning = relationship("WordMeaning", back_populates="sentences")
+
+
+class PosTagger(Base):
+    __tablename__ = "word_pos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    words = Column(String, unique=False, index=True)
+    pos = Column(String, unique=False, nullable=True, index=True)
+    word_meaning_id = Column(Integer, ForeignKey("word_meaning.id"))
+
+    word_meaning = relationship("WordMeaning", back_populates="pos_taggers")
+
+
+class IPA(Base):
+    __tablename__ = "ipa"
+
+    id = Column(Integer, primary_key=True, index=True)
+    words = Column(String, unique=False, index=True)
+    ipa = Column(String, unique=False, nullable=True, index=True)
+    word_meaning_id = Column(Integer, ForeignKey("word_meaning.id"))
+
+    word_meaning = relationship("WordMeaning", back_populates="ipas")
+
+
+
+class AudioData(Base):
+    __tablename__ = "word_audio"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, unique=False, nullable=True)
+    duration = Column(Float, unique=False, nullable=True)
+    audio_file = Column(LargeBinary, unique=False, nullable=True)
+    audio_type = Column(String, unique=False, nullable=True)
+    word_meaning_id = Column(Integer, ForeignKey("word_meaning.id"))
+
+    word_meaning = relationship("WordMeaning", back_populates="audio_data")
